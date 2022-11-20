6845c6ab28d9b5449f4deb5f0d57ca577b8f54db
Omolara5861 <debbiegterra@gmail.com>
2022-11-20 23:08:31 +0100

    chore: :hammer: Add custom script

---
+++ js/script.js
@@ -0,0 +1,52 @@
+let searchForm = document.querySelector('.search-form');
+
+
+
+window.onscroll = () => {
+
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+
+}
+
+window.onload = () => {
+
+    if (window.scrollY > 80) {
+        document.querySelector('.header .header-2').classList.add('active');
+    } else {
+        document.querySelector('.header .header-2').classList.remove('active');
+    }
+}
+
+function liveSearch() {
+    // Locate the card elements
+    let content = document.querySelectorAll('.searched-content')
+    // Locate the search input
+    let search_query = document.getElementById("search-box").value;
+    // Loop through the content
+    for (var i = 0; i < content.length; i++) {
+      // If the text is within the card...
+    if(content[i].textContent.toLowerCase()
+        // ...and the text matches the search query...
+        .includes(search_query.toLowerCase())) {
+          // ...remove the `.is-hidden` class.
+        content[i].classList.remove("is-hidden");
+    } else {
+        // Otherwise, add the class.
+        content[i].classList.add("is-hidden");
+    }
+    }
+}
+
+  // A little delay
+let typingTimer;
+let typeInterval = 500; // Half a second
+let searchInput = document.getElementById('search-box');
+
+searchInput.addEventListener('keyup', () => {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(liveSearch, typeInterval);
+});
